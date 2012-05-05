64ab937ea51866f0f4fc78796fc5a9c1c9e89b80
Cevric <cevric.ju@orange.fr>
2012-05-05 18:54:28 +0100

    Fini avec les pages statiques

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,16 @@
 class PagesController < ApplicationController
   def home
+  	@titre= "Acceuil"
   end
 
   def contact
+  	@titre= "Contact"
+  end
+
+  def about
+  	@titre= "About"
+  end
+
+  def test
   end
 end
